929d47c16d1b7ba4481f93e4d930cdea90788651
Matheus Miranda <matheusmiranda.batista@gmail.com>
2024-10-04 17:09:38 -0300

    Public, Private and protected

--- src/Metodo_e_Classe/Caneta.java
+++ src/Metodo_e_Classe/Caneta.java
@@ -1,37 +1,44 @@
 package Metodo_e_Classe;
 //Classe caneta
 public class Caneta {
+
     //Atributos
-    String modelo;
-    String cor;
-    float pontta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    public String cor;
+
+    //'private' so pode ser mechido pela propria classe
+    private float ponta;
+
+    /*'protected' so pode ser mechido pela propria classe
+    e pelas suas sub-classes(herança)
+     */
+    protected int carga;
+    protected boolean tampada;
 
     //metodos
-    void status() {
+    public void status() {
         System.out.println("Modelo: " + this.modelo);
         System.out.println(" uma caneta " + this.cor);
-        System.out.println("ponta: " + this.pontta);
+        System.out.println("ponta: " + this.ponta);
         System.out.println("carga: " + this.carga);
         System.out.println("Está tampada? " + this.tampada);
     }
 
-    void rabiscar(){
+    protected void rabiscar(){
         if (this.tampada == true) {
             System.out.println("erro! não pode rabiscar");
         } else {
-            System.out.println("estou rabiscando");
+            System.out.println("estou rabiscando ");
         }
     }
 
-    void tampar() {
+    protected void tampar() {
         //'this' é o nome do objeto que chamou, ou seja, 'c1'
         // é usada para modificar o atributo dentra da classe
         this.tampada = true;
     }
 
-    void destampar() {
+    protected void destampar() {
         this.tampada = false;
     }
 }
--- src/Metodo_e_Classe/teste.java
+++ src/Metodo_e_Classe/teste.java
@@ -4,11 +4,14 @@ public class teste {
     public static void main(String[] args) {
         //Instanciar a classe
        Caneta c1 = new Caneta(); // 'c1' é Objeto
-       c1.cor = "azul";
-       c1.pontta = 0.5f;
-       c1.destampar();
 
-       c1.status();
-       c1.rabiscar();
+        c1.modelo = "bic cristal";
+        c1.cor = "azul";
+        c1.ponta = 0.5; // vai dar erro pois ponta so pode ser alterado na classe caneta
+        c1.carga = 80;
+        c1.tampada = false;
+
+        c1.status();
+        c1.rabiscar();
     }
 }
